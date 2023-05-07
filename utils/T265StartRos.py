e9e75bee519b6e375616881905d5775418082bb5
CGC12123 <caiguoci@126.com>
2023-05-07 20:55:49 +0800

    尝试开新终端

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -3,11 +3,13 @@ import time
 
 os.system("chmod +x ./bash/T265_Start_1.sh")
 os.system("./bash/T265_Start_1.sh")
-time.sleep(3)
+# time.sleep(3)
 
-os.system("xterm -e 'chmod +x ./bash/T265_Start_2.sh'")
+os.system("open -a Terminal")
+os.system("chmod +x ./bash/T265_Start_2.sh")
 os.system("./bash/T265_Start_2.sh")
-time.sleep()
+# time.sleep(3)
 
-os.system("xterm -e 'chmod +x ./bash/T265_Start_3.sh'")
+os.system("open -a Terminal")
+os.system("chmod +x ./bash/T265_Start_3.sh")
 os.system("./bash/T265_Start_3.sh")
