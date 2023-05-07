c1837a6860b77b8874de629c2b12780b4475cac3
CGC12123 <caiguoci@126.com>
2023-05-07 22:32:14 +0800

    自启动尝试

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -1,15 +1,22 @@
 import os
 import time
+import subprocess
 
-os.system("chmod +x ./bash/T265_Start_1.sh")
-os.system("./bash/T265_Start_1.sh")
+sh1 = "./bash/T265_Start_1.sh"
+sh2 = "./bash/T265_Start_2.sh"
+sh3 = "./bash/T265_Start_3.sh"
+
+subprocess.Popen(['gnome-terminal', '-x', 'bash', '-c', sh1])
+subprocess.Popen(['gnome-terminal', '-x', 'bash', '-c', sh2])
+subprocess.Popen(['gnome-terminal', '-x', 'bash', '-c', sh3])
+
 # time.sleep(3)
 
-os.system("open -a Terminal")
-os.system("chmod +x ./bash/T265_Start_2.sh")
-os.system("./bash/T265_Start_2.sh")
-# time.sleep(3)
+# os.system("open -a Terminal")
+# os.system("chmod +x ./bash/T265_Start_2.sh")
+# os.system("./bash/T265_Start_2.sh")
+# # time.sleep(3)
 
-os.system("open -a Terminal")
-os.system("chmod +x ./bash/T265_Start_3.sh")
-os.system("./bash/T265_Start_3.sh")
+# os.system("open -a Terminal")
+# os.system("chmod +x ./bash/T265_Start_3.sh")
+# os.system("./bash/T265_Start_3.sh")
