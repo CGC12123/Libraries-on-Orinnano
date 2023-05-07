37e11841f0cc75f972f9f591e61581a99b640a45
CGC12123 <caiguoci@126.com>
2023-05-07 22:40:23 +0800

    尝试自启动

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -6,9 +6,9 @@ sh1 = "./bash/T265_Start_1.sh"
 sh2 = "./bash/T265_Start_2.sh"
 sh3 = "./bash/T265_Start_3.sh"
 
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1])
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2])
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3])
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 # time.sleep(3)
 
