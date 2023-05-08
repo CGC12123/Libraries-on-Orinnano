30b6b49b57cfbc7dcee5ab43e3e1c77871b54eb7
CGC12123 <caiguoci@126.com>
2023-05-08 22:40:19 +0800

    t265自启动更改绝对路径

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -3,18 +3,23 @@ import time
 import subprocess
 
 def T265_Start():
-    sh1 = "./bash/T265_Start_1.sh"
-    sh2_1 = "echo 123456 | sudo -S chmod -x ./bash/T265_Start_2.sh"
-    sh2_2 = "./bash/T265_Start_2.sh"
-    sh3 = "./bash/T265_Start_3.sh"
+    sh1 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_1.sh"
+    sh2_1 = "echo 123456 | sudo -S chmod +x /home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"
+    sh2_2 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"
+    sh3 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_3.sh"
 
     subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     time.sleep(3)
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2_1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', "ls"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    os.system(sh2_1)
     os.system(sh2_2)
 
     time.sleep(3)
 
     subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    time.sleep(3)
+
+T265_Start()
