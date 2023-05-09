a945efe676cb9e05ef8161cee046cc3946eab973
CGC12123 <caiguoci@126.com>
2023-05-09 20:41:42 +0800

    try fix bug

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -4,21 +4,21 @@ import subprocess
 
 def T265_Start():
     sh1 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_1.sh"
-    sh2_1 = "echo 123456 | sudo -S chmod +x /home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"
-    sh2_2 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"
+    sh2s = ["echo 123456 | sudo -S chmod +x /home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh",
+           "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"]
+    sh2 = "bash -c '{}'".format("; ".join(sh2s))
     sh3 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_3.sh"
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1, '--hold'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1, '--hold'], 
+                    stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2_1, '--hold'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    os.system(sh2_2)
-
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2, '--hold'], 
+                    stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3, '--hold'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3, '--hold'], 
+                    stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
 
 T265_Start()
