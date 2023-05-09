b429336b41f1f242b240a18181df9fa13db72837
CGC12123 <caiguoci@126.com>
2023-05-09 19:57:35 +0800

    尝试新的自启动开终端方式

--- utils/T265StartRos.py
+++ utils/T265StartRos.py
@@ -8,17 +8,28 @@ def T265_Start():
     sh2_2 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_2.sh"
     sh3 = "/home/c/Library/Cv_for_Orinnano/utils/bash/T265_Start_3.sh"
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    time.sleep(3)
+    # time.sleep(3)
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2_1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh2_1], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # os.system(sh2_2)
+
+    # time.sleep(3)
+
+    # subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # time.sleep(3)
+
+    os.system('gnome-terminal')
+    os.system(sh1)
+
+    os.system('gnome-terminal')
+    os.system(sh2_1)
     os.system(sh2_2)
 
-    time.sleep(3)
+    os.system('gnome-terminal')
+    os.system(sh3)
 
-    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', sh3], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    time.sleep(3)
 
 T265_Start()
