e6553af91d25f01f8f17c4cad650037a85253082
CGC12123 <caiguoci@126.com>
2023-05-10 10:50:02 +0800

    启动总入口

--- Start_system.py
+++ Start_system.py
@@ -1,6 +1,7 @@
 '''
+启动总入口
 启动前双目重新拔插 可上电后再插入
-USB0及USB1为两个ch340用于与飞控通信，其中一个走双目，一个走cv
+USB0及USB1为两个ch340用于与飞控通信，其中0走双目，1走cv
 '''
 from utils.T265StartRos import T265_Start
 import os
@@ -14,6 +15,5 @@ cv_shs = ["echo 123456 | sudo -S chmod 777 /dev/ttyUSB1",
           "conda activate py37", 
           "python /home/c/Library/Cv_for_Orinnano/find_color.py"]
 cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
-
 
 subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
