1269d1f3356e4947a4e763e4c70685dc435164eb
CGC12123 <caiguoci@126.com>
2023-05-11 20:23:46 +0800

    启动总入口

--- Start_system.py
+++ Start_system.py
@@ -11,9 +11,14 @@ import subprocess
 T265_Start()
 
 # 执行cv
-cv_shs = ["echo 123456 | sudo -S chmod 777 /dev/ttyUSB1",
-          "conda activate py37", 
-          "python /home/c/Library/Cv_for_Orinnano/find_color.py"]
-cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
+# cv_shs = ["condaenv",
+#           "conda activate py37", 
+#           "python /home/c/Library/Cv_for_Orinnano/main.py"]
+# cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
 
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+os.system("export PATH=/home/c/archiconda3/bin:$PATH")
+os.system("source ~/.bashrc")
+os.system("condaenv")
+os.system("conda activate py37")
+os.system("python /home/c/Library/Cv_for_Orinnano/Cv_Mode.py")
