0d55aecb4930a074d31eca2f5f29c01fdaf3acba
CGC12123 <caiguoci@126.com>
2023-07-03 16:17:53 +0800

    完善cv自启动环节

--- Start_system.py
+++ Start_system.py
@@ -4,6 +4,7 @@
 USB0及USB1为两个ch340用于与飞控通信，其中0走双目，1走cv
 '''
 from utils.T265StartRos import T265_Start
+from utils.CvStart import Cv_Start
 import os
 import subprocess
 
@@ -11,11 +12,4 @@ import subprocess
 T265_Start()
 
 # 执行cv
-cv_shs = ["python /home/c/Library/Cv_for_Orinnano/Cv_mode.py"]
-cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# os.system("export PATH=/home/c/archiconda3/bin:$PATH")
-# os.system("source ~/.bashrc")
-# os.system("condaenv")
-# os.system("conda activate py37")
-# os.system("python /home/c/Library/Cv_for_Orinnano/Cv_Mode.py")
+Cv_Start()
---
+++ utils/CvStart.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def Cv_Start():
+    cv_shs = ["echo 123456 | sudo -S chmod +x /home/c/Library/Cv_for_Orinnano/utils/bash/CvModeStart.sh",
+              "/home/c/Library/Cv_for_Orinnano/utils/bash/CvModeStart.sh"]
+    cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], 
+                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+if __name__ == '__main__':
+    Cv_Start()
