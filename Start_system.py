0d3c089042a8c1ed2cefbd8b16a191d21c22b331
CGC12123 <caiguoci@126.com>
2023-07-03 15:52:10 +0800

    cvmode test

--- Start_system.py
+++ Start_system.py
@@ -11,14 +11,11 @@ import subprocess
 T265_Start()
 
 # 执行cv
-# cv_shs = ["condaenv",
-#           "conda activate py37", 
-#           "python /home/c/Library/Cv_for_Orinnano/main.py"]
-# cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
-
-# subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-os.system("export PATH=/home/c/archiconda3/bin:$PATH")
-os.system("source ~/.bashrc")
-os.system("condaenv")
-os.system("conda activate py37")
-os.system("python /home/c/Library/Cv_for_Orinnano/Cv_Mode.py")
+cv_shs = ["python /home/c/Library/Cv_for_Orinnano/Cv_mode.py"]
+cv_sh = "bash -c '{}'".format("; ".join(cv_shs))
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', cv_sh], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# os.system("export PATH=/home/c/archiconda3/bin:$PATH")
+# os.system("source ~/.bashrc")
+# os.system("condaenv")
+# os.system("conda activate py37")
+# os.system("python /home/c/Library/Cv_for_Orinnano/Cv_Mode.py")
---
+++ utils/bash/CvModeStart.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python /home/c/Library/Cv_for_Orinnano/Cv_mode.py
