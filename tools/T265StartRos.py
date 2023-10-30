3c6bf06cdbf111b8774596ff5d3fc115afb83397
CGC12123 <caiguoci@126.com>
2023-10-30 18:10:40 +0800

    加入t265启动多线程

--- tools/T265StartRos.py
+++ tools/T265StartRos.py
@@ -1,6 +1,7 @@
 import os
 import time
 import subprocess
+import threading
 
 def T265_Start():
     sh1 = "/home/c/Library/Cv_for_Orinnano/tools/bash/T265_Start_1.sh"
@@ -22,5 +23,35 @@ def T265_Start():
                     stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(5)
 
+def T265_Start_Thread1():
+    os.system("/home/c/Library/Cv_for_Orinnano/tools/bash/T265_Start_1.sh")
+
+def T265_Start_Thread2():
+    os.system("echo 123456 | sudo -S chmod +x /home/c/Library/Cv_for_Orinnano/tools/bash/T265_Start_2.sh")
+    os.system("sudo chmod 777 /dev/ttyUSB0")
+    os.system("/home/c/Library/Cv_for_Orinnano/tools/bash/T265_Start_2.sh")
+
+def T265_Start_Thread3():
+    os.system("/home/c/Library/Cv_for_Orinnano/tools/bash/T265_Start_3.sh")
+
+def T265_ThreadStart():
+    # 创建线程对象
+    thread1 = threading.Thread(target=T265_Start_Thread1)
+    thread2 = threading.Thread(target=T265_Start_Thread2)
+    thread3 = threading.Thread(target=T265_Start_Thread3)
+
+    thread1.start()
+    time.sleep(5)
+    thread2.start()
+    time.sleep(5)
+    thread3.start()
+    time.sleep(5)
+
+    # 等待线程结束
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
 if __name__ == '__main__':
-    T265_Start()
+    # T265_Start()
+    T265_ThreadStart()
